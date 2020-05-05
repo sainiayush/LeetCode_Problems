1bdb7411179026ece8ce2978d7dfaade36230212
Prerak Raja <rajaprerak@gmail.com>
2020-05-05 21:33:11 +0530

    linked list problems added

--- Linked List/Readme.md
+++ Linked List/Readme.md
@@ -1,4 +1,6 @@
 #### Problems
 |  Number | Problem |   Solution |
 | --- | --- | --- |
-|  1290 | [Convert Binary Number in a Linked List to Integer](https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/) | [solution](/Hash%20Table/convert_binary_Number_in_linked_list_to_integer.py)|
+|  206 | [Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/) | [solution](/Linked%20List/reverse_linked_list.py)|
+|  876 | [Middle of the Linked List](https://leetcode.com/problems/middle-of-the-linked-list/) | [solution](/Linked%20List/middle_of_the_linked_list.py)|
+|  1290 | [Convert Binary Number in a Linked List to Integer](https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/) | [solution](/Linked%20List/convert_binary_Number_in_linked_list_to_integer.py)|
---
+++ Linked List/middle_of_the_linked_list.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+
+    def middleNode(self, head: ListNode) -> ListNode:
+
+        ## Efficient solution
+        first = second = head
+        while second and second.next:
+            first = first.next
+            second = second.next.next
+        return first
+
+#         node = head
+#         count = 0
+#         while head is not None:
+#             count+=1
+#             head = head.next
+#         if count%2 == 0 :
+#             index = (count/2)
+#         else:
+#             index = count//2
+
+#         temp = 0
+#         while True:
+#             if temp<index:
+#                 node = node.next
+#             else:break
+#             temp += 1
+#         return node
