a870a6d2271dfced953a23b98de97248aa509328
Prerak Raja <rajaprerak@gmail.com>
2021-05-25 21:09:07 -0700

    Create is-subsequence.py

---
+++ Solutions/is-subsequence.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        s1,t1=0,0
+        while s1<len(s) and t1<len(t):
+            if s[s1] == t[t1]:s1 = s1+1
+            t1 = t1+1
+        return len(s) == s1
+                
+        
+        
+        
